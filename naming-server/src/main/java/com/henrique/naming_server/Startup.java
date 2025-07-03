6ce317aaf40e124b5c2efbec1f3b3149265373e3
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2025-07-03 12:07:09 -0300

    feat(naming-service): adds eureka service discovery

---
+++ naming-server/src/main/java/com/henrique/naming_server/Startup.java
@@ -0,0 +1,15 @@
+package com.henrique.naming_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
