1b24556a21a553c30c237b4bfbff0cebf72720da
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2025-07-07 19:35:05 -0300

    feat(book-service): adds resilience4j retry, circuit breaker and ratelimiter

---
+++ book-service/src/main/java/com/henrique/book_service/controller/FoobarController.java
@@ -0,0 +1,39 @@
+package com.henrique.book_service.controller;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FoobarController {
+
+    private final Logger logger = LoggerFactory.getLogger(FoobarController.class);
+
+    @GetMapping("/foo-bar")
+    /*
+        @Retry(name = "foo-bar") //retries amount of times configured on yml file
+        @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // same as before but has a fallback method
+        @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+        @RateLimiter(name = "default")
+    */
+    @Bulkhead(name = "default")
+    public String fooBar() {
+        logger.info("Request to foo-bar received!");
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+        return "FOO BAR!!";
+    }
+
+    public String fallbackMethod(Exception ex) {
+        return "fallbackMethod foo-bar";
+    }
+}
