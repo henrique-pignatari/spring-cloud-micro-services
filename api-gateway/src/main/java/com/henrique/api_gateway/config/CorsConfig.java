6ba8aff3c57c9f0ab85268aa4ad25a373a48ab12
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2025-07-08 00:54:21 -0300

    feat(all-services): adds zipkin distributed tracing

--- api-gateway/src/main/java/com/henrique/api_gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/henrique/api_gateway/config/CorsConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
--- exchange-service/src/main/java/com/henrique/exchange_service/controller/ExchangeController.java
+++ exchange-service/src/main/java/com/henrique/exchange_service/controller/ExchangeController.java
@@ -5,6 +5,8 @@ import com.henrique.exchange_service.model.Exchange;
 import com.henrique.exchange_service.repository.ExchangeRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,8 @@ import java.math.BigDecimal;
 @RequestMapping("exchange-service")
 public class ExchangeController {
 
+    private final Logger logger = LoggerFactory.getLogger(ExchangeController.class);
+
     private final InstanceInformationService informationService;
     private final ExchangeRepository exchangeRepository;
 
@@ -29,6 +33,7 @@ public class ExchangeController {
     @Operation(summary = "Get an exchange from amount of currency")
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
+        logger.info("getExchange is called with -> {}, {} and {}", amount, from, to);
         Exchange exchange = exchangeRepository.findByFromAndTo(from, to)
                 .orElseThrow(() -> new RuntimeException("could not find an exchange from: " + from + "to: " + to));
         BigDecimal conversionFactor = exchange.getConversionFactor();
